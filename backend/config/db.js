6b54f5136caa4777d1741ee771a1fcd52db18c3c
ragulk12 <raguldecrz@gmail.com>
2024-08-27 14:15:59 +0530

    server update

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,8 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // 5 seconds
+      socketTimeoutMS: 45000, 
     });
     console.log('Connected to MongoDB');
   } catch (error) {
